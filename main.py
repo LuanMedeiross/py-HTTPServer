749a2e784ddd28af2a5da284ad8af8195e77c574
LuanMedeiross <veryglue@gmail.com>
2025-02-16 19:56:34 -0300

    Adicionado método 'start' na classe TCPServer para receber conexões tcp e método 'handle_request' para lidar com as requisições

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import socket
 
-class HTTPServer:
+class TCPServer:
 
-    def __init__(self, port, ip):
+    def __init__(self, host, port):
         self.port = port
-        self.ip = ip
+        self.host = host
 
+    def start(self):
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((self.host, self.port))
+        sock.listen(5)
+        
+        print("Listening at", sock.getsockname())
+
+        while True:
+           
+            conn, addr = sock.accept()
+
+            print("Connected by", addr)
+
+            data = conn.recv(2048)
+
+            response = self.handle_request(data)
+
+            conn.sendall(response)
+            conn.close()
+
+    def handle_request(self, data):
+        # Do whatever with the data
+        return data
     
+
+
+if __name__ == "__main__":
+
+    http = TCPServer('192.168.0.13', 3000)
+    http.start()
